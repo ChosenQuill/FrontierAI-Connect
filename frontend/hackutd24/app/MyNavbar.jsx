3256b24daac4f226a985b78b0ecdf5626e41fb22
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2024-11-17 11:14:17 -0600

    moved log in option
    Co-authored-by: atc6505 <atc6505@users.noreply.github.com>

--- frontend/hackutd24/app/MyNavbar.jsx
+++ frontend/hackutd24/app/MyNavbar.jsx
@@ -1,14 +1,15 @@
 "use client";
 
 import Image from "next/image";
-
+import Link from "next/link";
 // pages/index.js
 import { useState } from 'react';
 // Import shadcn/ui components as needed
 
-export default function MyNavbar() {
-    return <div className="border-1 border-black rounded-3xl w-full h-16 bg-white absolute top-0 flex items-center">
-      <Image src="/logo.svg" className="w-12 h-12 mx-2 inline" width={12} height={12}/>
+export default function MyNavbar(props) {
+    return <div className="border-1 border-solid border-black rounded-3xl w-full h-16 bg-white absolute top-0 flex items-center">
+      <Image alt="logo" src="/logo.svg" className="w-12 h-12 mx-2 inline" width={12} height={12}/>
       <h1 className="inline mx-auto text-2xl font-bold font-sans text-slate-600" >Here you can find what you need!</h1>
+      {!props.displayForm && <button className="bg-gray-100 rounded w-20 h-8 m-6 inline float-right" onClick={props.handleClickForm}>Log In</button>}
     </div>;
 }
