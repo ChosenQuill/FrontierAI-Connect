898e7e0e56c10626f7501c2c52c50fb9ae61a485
hadent dang <hadentdang@gmail.com>
2024-11-17 05:51:20 -0600

    updated buttons for log in maps and search. positions/visuals
    Co-authored-by: atc6505 <atc6505@users.noreply.github.com>

--- frontend/hackutd24/app/page2.jsx
+++ frontend/hackutd24/app/page2.jsx
@@ -14,13 +14,22 @@ export default function Page2() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [displayImage, setDisplayImage] = useState(false);
+  const [displayForm, setDisplayForm] = useState(false);
 
-  function handleClick() { 
-    setDisplayImage(!displayImage); 
+
+  function handleClickImage() { 
+    if(!displayForm)
+      setDisplayImage(!displayImage); 
+  }
+  function handleClickForm() { 
+    if(!displayImage)
+      setDisplayForm(!displayForm); 
   }
 
+  function onSubmit(){
+    let x = document.getElementById("Username");
+  }
   
-
   return (<>
   <div id='test' className={ "bg-slate-700 min-h-screen flex flex-col px-4 "}> {/*styles.frontierImage*/}
     {/*<div className="w-full max-w-md">
@@ -29,10 +38,22 @@ export default function Page2() {
         {error && <p className="mt-4 text-center text-red-500">{error}</p>}
       </div>*/}
       <div className="inline object-top mx-auto">
-        <button className="bg-gray-100 rounded w-20 h-8 m-6">button1</button>
-        <button className="bg-gray-100 rounded w-20 h-8 m-6" onClick={handleClick}> Locations </button>
+        <button className="bg-gray-100 rounded w-20 h-8 m-6 inline" onClick={handleClickForm}>Log In</button>
+        <button className="m-6 inline" onClick={handleClickImage}> <Image src="/red pin.PNG" className="bg-slate-400 mx-auto my-8 border-4 border-black rounded-2xl inline" width={100} height={100}/> </button>
       </div>
-      {displayImage && <div className="allign-center"><Image src="/map.PNG" className="w-1200 h-1200 mx-auto my-8 border-2 border-black rounded-2xl" width={1200} height={1200}/></div>} 
+      {displayImage && <div className="allign-center">
+        <Image src="/map.PNG" className="w-1200 h-1200 mx-auto my-8 border-4 border-black rounded-2xl" width={1200} height={1200}/>
+        </div>}
+      {
+        displayForm && <form>
+          <label for="username" required>Username: </label>
+          <input type="text" id="username"/>
+          <label for="password" required>Password: </label>
+          <input type="password" id="password"/>
+          <button type="submit">Submit</button>
+        </form>
+      }
+       
     </div>
    
     </>
