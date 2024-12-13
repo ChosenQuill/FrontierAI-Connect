c5d521dccd9180bf810cdbdaa273013b5dab7f8f
Rithvik S <31707957+ChosenQuill@users.noreply.github.com>
2024-12-13 03:17:51 -0600

    Updated Project with altest information + bugfix v2

--- frontend/app/MyNavbar.jsx
+++ frontend/app/MyNavbar.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 // Import shadcn/ui components as needed
 
 export default function MyNavbar(props) {
-    return <div className="border-1 border-solid border-black rounded-3xl w-full h-16 bg-white absolute top-0 flex items-center">
+    return <div className="border-1 border-solid border-black rounded-3xl w-full h-16 bg-white my-4 top-0 flex items-center">
       <Image alt="logo" src="/logo.svg" className="w-12 h-12 mx-2 inline" width={12} height={12}/>
       <h1 className="inline mx-auto text-2xl font-bold font-sans text-slate-600" >Here you can find what you need!</h1>
       {!props.displayForm && <button className="bg-gray-100 rounded w-20 h-8 m-6 inline float-right" onClick={props.handleClickForm}>Log In</button>}
